8f25d5735443ca96e080d992a289e258a0fff9e6
Katrina Lyman <lymankatrina@gmail.com>
2023-03-20 22:38:58 -0600

    Finished week 1 activities

--- week1/childNodes.js
+++ week1/childNodes.js
@@ -1,17 +1,14 @@
-// url for church website
-const url = "https://www.churchofjesuschrist.org/?lang=eng";
-
 // shows children of document.body
 for (let i = 0; i < document.body.childNodes.length; i++) {
-  return document.body.childNodes[i];
+	return document.body.childNodes[i];
 }
 
 // shows all nodes from the collection
 for (let node of document.body.childNodes) {
-  return node;
+	return node;
 }
 
 // show only elements
 for (let elem of document.body.children) {
-  return elem;
+	return elem;
 }
