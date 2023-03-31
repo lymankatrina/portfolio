05638e780c7b5f9e8718b38923cf99b34ce4765a
Katrina Lyman <lymankatrina@gmail.com>
2023-03-31 08:49:42 -0600

    try this path for header image

--- final-project/js/header.js
+++ final-project/js/header.js
@@ -1,4 +1,4 @@
 mainHeader.innerHTML = `
-<div class="header-container"><img class="logo" src="final-project/img/book-logo.svg" alt="book logo">
+<div class="header-container"><img class="logo" src="../final-project/img/book-logo.svg" alt="book logo">
 <h1 class="header-text">Book Finder</h1></div>
 `;
