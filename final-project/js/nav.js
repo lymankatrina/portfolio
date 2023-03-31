8c13495b9956302fa12d0b7d34d87b72fbb20d12
Katrina Lyman <lymankatrina@gmail.com>
2023-03-31 08:42:15 -0600

    fix links for github page to work

--- final-project/js/nav.js
+++ final-project/js/nav.js
@@ -1,5 +1,5 @@
 mainNav.innerHTML = `
 <ul>
-  <li class="btn"><a href="/final-project/index.html">Home</a></li>
-  <li vlass="btn"><a href="/final-project/advanced/index.html">Advanced Search</a></li>
+  <li class="btn"><a href="final-project/index.html">Home</a></li>
+  <li vlass="btn"><a href="final-project/advanced/index.html">Advanced Search</a></li>
 </ul>`;
