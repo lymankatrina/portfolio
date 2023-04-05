16b66325990086df02fc258d02d40fadd3525675
Katrina Lyman <lymankatrina@gmail.com>
2023-04-05 14:53:19 -0600

    links

--- final-project/js/nav.js
+++ final-project/js/nav.js
@@ -1,5 +1,5 @@
 mainNav.innerHTML = `
 <ul>
-  <li class="btn"><a href="./portfolio/final-project/index.html">Basic Search</a></li>
-  <li class="btn"><a href="./portfolio/final-project/advanced/index.html">Advanced Search</a></li>
+  <li class="btn"><a href="/portfolio/final-project/index.html">Basic Search</a></li>
+  <li class="btn"><a href="/portfolio/final-project/advanced/index.html">Advanced Search</a></li>
 </ul>`;
