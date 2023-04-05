5e7b7b80cea012f6cd4c9feffc8c60b77187e7c9
Katrina Lyman <lymankatrina@gmail.com>
2023-04-05 13:41:13 -0600

    try to fix links

--- final-project/js/nav.js
+++ final-project/js/nav.js
@@ -1,5 +1,5 @@
 mainNav.innerHTML = `
 <ul>
-  <li class="btn"><a href="/final-project/index.html">Basic Search</a></li>
-  <li class="btn"><a href="/final-project/advanced/index.html">Advanced Search</a></li>
+  <li class="btn"><a href="../final-project/index.html">Basic Search</a></li>
+  <li class="btn"><a href="../final-project/advanced/index.html">Advanced Search</a></li>
 </ul>`;
