412037257ab6a20c5d874d44d991099ea6b837bd
Katrina Lyman <lymankatrina@gmail.com>
2023-04-05 15:00:33 -0600

    try this

--- final-project/js/nav.js
+++ final-project/js/nav.js
@@ -1,5 +1,5 @@
 mainNav.innerHTML = `
 <ul>
-  <li class="btn"><a href="<portfolio>/final-project/index.html">Basic Search</a></li>
-  <li class="btn"><a href="<portfolio>/final-project/advanced/index.html">Advanced Search</a></li>
+  <li class="btn"><a href=__DIR__."/final-project/index.html">Basic Search</a></li>
+  <li class="btn"><a href=__DIR__."/final-project/final-project/advanced/index.html">Advanced Search</a></li>
 </ul>`;
