9708e0ed21f3065cd602c22235838659955d5cb6
Katrina Lyman <lymankatrina@gmail.com>
2023-04-05 13:45:21 -0600

    fix links in nav and header

--- final-project/js/nav.js
+++ final-project/js/nav.js
@@ -1,5 +1,5 @@
 mainNav.innerHTML = `
 <ul>
-  <li class="btn"><a href="../final-project/index.html">Basic Search</a></li>
-  <li class="btn"><a href="../final-project/advanced/index.html">Advanced Search</a></li>
+  <li class="btn"><a href="https://lymankatrina.github.io/portfolio.github.io/final-project/index.html">Basic Search</a></li>
+  <li class="btn"><a href="https://lymankatrina.github.io/portfolio.github.io/final-project/advanced/index.html">Advanced Search</a></li>
 </ul>`;
